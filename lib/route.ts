83392bce28c5e3e1cfc66f198b85968bdb25474d
Can Gündüz <cangunduz0001@gmail.com>
2025-03-29 00:32:58 +0300

    Navigation | 1.0.2

---
+++ lib/route.ts
@@ -0,0 +1,34 @@
+// type RouteAccessProps = {
+//     [key: string]: string[];
+// };
+
+import { createRouteMatcher } from "@clerk/nextjs/server";
+
+// export const routeAccess: RouteAccessProps = {
+//     "/admin(.*)" :['admin'],
+//     "/patient(.*)" :['admin','patient','doctor'],
+// }
+
+
+export const routeMatchers = {
+    admin: createRouteMatcher([
+        "/admin(.*)",
+        "/patient(.*)",
+        "/record/users",
+        "/record/doctors(.*)",
+        "/record/patients",
+        "/record/doctors",
+        "/record/staffs",
+        "/record/patients",
+    ]),
+    patient: createRouteMatcher(["/patient(.*)","/patient/registrations"]),
+
+    doctor: createRouteMatcher([
+        "/doctor(.*)",
+        "/record/doctors(.*)",
+        "/record/patients",
+        "/patient(.*)",
+        "/record/staffs",
+        "/record/patients",
+    ]),
+}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,28 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { routeMatchers } from "./lib/route";
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const checkRoleAndRedirect = (
+  req: NextRequest,
+  role: string | undefined,
+  allowedRole: keyof typeof routeMatchers
+) : NextResponse | undefined => {
+  if(routeMatchers[allowedRole](req) && role !== allowedRole) {
+    const url = new URL("/",req.url);
+    console.log("Redirecting to /",url);
+    return NextResponse.redirect(url);
+  }
+};
+
+export default clerkMiddleware(async (auth, req) => {
+  const {userId, sessionClaims} = await auth();
+  const role = (sessionClaims?.metadata as {role?: string})?.role;
+  const response = 
+  checkRoleAndRedirect(req, role, "admin") ||
+  checkRoleAndRedirect(req, role, "doctor");
+
+  if(response) return response;
+});
 
 export const config = {
   matcher: [
